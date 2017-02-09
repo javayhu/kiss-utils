f29a953c8721d601b7156b15f3a3c7f77b12d8b8
hujiawei <hujiawei090807@gmail.com>
2017-02-09 10:12:14 +0800

    Initial commit

---
+++ example/src/main/java/com/javayhu/kiss/MainActivity.java
@@ -0,0 +1,23 @@
+package com.javayhu.kiss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.javayhu.kiss.utils.AppUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoUtils(View view) {
+        String name = AppUtils.getAppPackageName(this);
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText(name);
+    }
+}
