d41f0455e694f009550a5df43a52b67a4735fe78
hujiawei <hujiawei090807@gmail.com>
2017-02-09 11:32:19 +0800

    update: update document, proguard and demo

--- example/src/main/java/com/javayhu/kiss/MainActivity.java
+++ example/src/main/java/com/javayhu/kiss/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.javayhu.kiss.utils.AppUtils;
+import com.javayhu.kiss.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Utils.init(this);
     }
 
     public void gotoUtils(View view) {
--- library/proguard-rules.pro
+++ library/proguard-rules.pro
@@ -15,3 +15,7 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+
+-keep class com.javayhu.kiss.utils.** { *; }
+-keepclassmembers class com.javayhu.kiss.utils.** { *; }
+-dontwarn com.javayhu.kiss.utils.**
